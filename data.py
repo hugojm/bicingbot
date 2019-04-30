f778aff5b3bfffdeacf092fc84a315aa551b145a
Hugo Jimenez Muñoz <hugo.jimenez@client69-10-192-69.eduroam.upc.edu>
2019-04-30 08:42:25 +0200

    Lo del bot de telegram va en bot.py

--- data.py
+++ data.py
@@ -1,49 +1,21 @@
-<<<<<<< HEAD
 import pandas as pd
 import networkx as nx
 from pandas import DataFrame
 from haversine import haversine
 from geopy.geocoders import Nominatim
 
-def authors():
-    print("The authors of these fabulous project are:")
-    print("Hugo Jiménez, whose email adress is hugo.jimenez@est.fib.upc.edu")
-    print("Jaume Martínez, whose email adress is jaume.martinez.ara@est.fib.upc.edu")
 
-def graph(G):
-    n = read(int)
-    #the new graph will have distance n
-    # G = ... (the graph with distance n)
+def data():
+    dataset = "https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information"
+    bicing = DataFrame.from_records(pd.read_json(dataset)['data']['stations'], index='station_id')
+    print(bicing)
 
-# prints the connex components of G
-def connex(G):
-    ...
-
-# plots the map with all the bicing stations and their connections
-def plotgraph(G):
-    ...
-
-def main():
-    G = nx.Graph()
-    accio = read(string)
-    while accio is not None:
-        if (accio == "/start"):
-            print("Hello")
-        else if (accio == "/authors"):
-            authors()
-        else if (accio == "/graph"):
-            graph(G)
-        else if (accio == "/nodes"):
-            print(G.number_of_nodes())
-        else if (accio == "/edges"):
-            print(G.number_of_edges())
-        else if (accio == "/components"):
-            connex()
-        else if (accio == "/plotgraph"):
-            plotgraph()
-=======
-def main():
-hujdshds
-
-njssndj
->>>>>>> 1680a84fec370aeece46abc5e63baa0de2feaaba
+def addressesTOcoordinates(addresses):
+    try:
+        geolocator = Nominatim(user_agent="bicing_bot")
+        address1, address2 = addresses.split(',')
+        location1 = geolocator.geocode(address1 + ', Barcelona')
+        location2 = geolocator.geocode(address2 + ', Barcelona')
+        return (location1.latitude, location1.longitude), (location2.latitude, location2.longitude)
+    except:
+        return None
