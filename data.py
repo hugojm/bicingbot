9776d426f6e346dfbc06acd65445513edffc8470
Hugo Jimenez Muñoz <hugo.jimenez@client9-10-192-30.eduroam.upc.edu>
2019-05-09 14:45:26 +0200

    ahi vamos

--- data.py
+++ data.py
@@ -3,10 +3,11 @@ import networkx as nx
 from pandas import DataFrame
 from haversine import haversine
 import matplotlib.pyplot as plt
-from staticmap import StaticMap, CircleMarker
+from staticmap import StaticMap, CircleMarker, Line
 
 
 def data():
+    distance = float(input("Distance "))
     """options = {
         'node_color': 'black',
         'node_size': 10,
@@ -20,18 +21,30 @@ def data():
     G = nx.Graph()
     for st in bicing.itertuples():
         G.add_node(st.Index, pos=(st.lon, st.lat))
-        for st2 in bicing.itertuples():
+    pos=nx.get_node_attributes(G,'pos')
+    for nod in G.nodes():
+        for nod2 in G.nodes():
+            pos=nx.get_node_attributes(G,'pos')
+            if (haversine(pos[nod],pos[nod2]) <= distance):
+                G.add_edge(nod,nod2)
+    print(G.number_of_nodes())
+    print(G.number_of_edges ())
+    print(haversine(pos[303],pos[338]))
     return G
     # pos = nx.get_node_attributes(G, 'pos')
     # nx.draw(G, pos, **options)
     # plt.savefig("path.png")
 
 def print_map(G):
-    distance = input("Distance ")
     m = StaticMap(800, 800)
     for n in list(G.nodes(data=True)):
-        marker = CircleMarker(n[1]['pos'], 'red', 4)
+        marker = CircleMarker(n[1]['pos'], 'red', 6)
         m.add_marker(marker)
+    for n2 in G.edges():
+        pos=nx.get_node_attributes(G,'pos')
+        coordinates = [pos[n2[0]],pos[n2[1]]]
+        line = Line(coordinates, 'blue', 1)
+        m.add_line(line)
     image = m.render()
     image.save('map.png')
 
@@ -48,5 +61,4 @@ def addressesTOcoordinates(addresses):
         return None
 
 
-data()
 print_map(data())
