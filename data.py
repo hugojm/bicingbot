ccfa33296e5bf206b4b1d7087fe5ba3627cfb726
Hugo Jimenez Muñoz <hugo.jimenez@client9-10-192-30.eduroam.upc.edu>
2019-05-09 12:52:18 +0200

    ahi vamos

--- data.py
+++ data.py
@@ -2,28 +2,41 @@ import pandas as pd
 import networkx as nx
 from pandas import DataFrame
 from haversine import haversine
-from geopy.geocoders import Nominatim
 import matplotlib.pyplot as plt
+from staticmap import StaticMap, CircleMarker
 
-hola sapo
+
 def data():
-    options = {
+    """options = {
         'node_color': 'black',
         'node_size': 10,
         'width': 3,
-     }
-
+    }
+    """
     dataset = "https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information"
-    bicing = DataFrame.from_records(pd.read_json(dataset)['data']['stations'], index='station_id')
-    print(bicing)
+    bicing = DataFrame.from_records(
+        pd.read_json(dataset)['data']['stations'],
+        index='station_id')
     G = nx.Graph()
     for st in bicing.itertuples():
-        cord1 = (st.lat , st.lon)
-        G.add_node(st.Index, pos=(-st.lat, -st.lon))
-        pos=nx.get_node_attributes(G,'pos')
+        G.add_node(st.Index, pos=(st.lon, st.lat))
+    return G
+    # pos = nx.get_node_attributes(G, 'pos')
+    # nx.draw(G, pos, **options)
+    # plt.savefig("path.png")
 
-    nx.draw(G,pos,**options)
-    plt.savefig("path.png")
+def print_map(G):
+    m = StaticMap(800, 800)
+    for n in list(G.nodes(data=True)):
+        marker = CircleMarker(n[1]['pos'], 'red', 4)
+        m.add_marker(marker)
+        coord1 = n[1]['pos']
+        coord2 = n[2]['pos']
+        if ((haversine(coord1, coord2)) < d):
+            line = Line([coord1,coord2], '#D2322D', 4)
+            m.add_line(line)
+    image = m.render()
+    image.save('map.png')
 
 
 def addressesTOcoordinates(addresses):
@@ -32,10 +45,11 @@ def addressesTOcoordinates(addresses):
         address1, address2 = addresses.split(',')
         location1 = geolocator.geocode(address1 + ', Barcelona')
         location2 = geolocator.geocode(address2 + ', Barcelona')
-        return (location1.latitude, location1.longitude), (location2.latitude, location2.longitude)
-    except:
+        return (location1.latitude,
+                location1.longitude), (location2.latitude, location2.longitude)
+    except BaseException:
         return None
 
 
-
 data()
+print_map(data())
